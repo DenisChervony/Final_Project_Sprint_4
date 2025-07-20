740dd9cf5ce3ebdad78a8407223dcfbdfa52b60c
Червоный Денис <chervony.denis@ya.ru>
2025-07-20 13:03:52 +0500

    Добавленна ветка develop и структура проекта

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,4 +1,95 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class MainPage {
+    private final WebDriver driver;
+
+    // Локаторы
+    private final By cookieButton = By.id("rcc-confirm-button"); // Кнопка согласия с куки
+
+    /* В задании сказано "Обрати внимание: в приложении есть баг, который не даёт оформить заказ.
+    Он воспроизводится только в Chrome."
+    Локатор нижней кнопки "Заказать" срабатывает*/
+    private final By orderButtonTop = By.xpath(".//button[text()='Заказать' and @class='Button_Button__ra12g']"); // Верхняя кнопка "Заказать"
+    private final By orderButtonBottom = By.xpath("//div[contains(@class, 'Home_FinishButton')]/button[text()='Заказать']"); // Нижняя кнопка "Заказать"
+    private final By faqQuestion = By.xpath("//div[@data-accordion-component='AccordionItem']"); // Вопросы FAQ
+    private final By faqAnswer = By.xpath("//div[@data-accordion-component='AccordionItemPanel']"); // Ответы FAQ
+    private final By scooterLogo = By.xpath("//a[@class='Header_LogoScooter__3lsAR']"); // Логотип Самоката
+    private final By yandexLogo = By.xpath("//a[@class='Header_LogoYandex__3TSOI']"); // Логотип Яндекса
+    private final By statusOrderButton = By.xpath("//button[text()='Статус заказа']"); // Кнопка "Статус заказа"
+    private final By orderNumberField = By.xpath("//input[@placeholder='Введите номер заказа']"); // Поле ввода "Введите номер заказа"
+    private final By goButton = By.xpath("//button[text()='Go!']"); // Кнопка Go!
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickCookieButton() {
+        driver.findElement(cookieButton).click();
+    }
+
+    public void clickOrderButtonTop() {
+        driver.findElement(orderButtonTop).click();
+    }
+
+    public void clickOrderButtonBottom() {
+        WebElement element = driver.findElement(orderButtonBottom);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    public void clickFaqQuestion(int index) {
+        driver.findElements(faqQuestion).get(index).click();
+    }
+
+    public String getFaqAnswer(int index) {
+        return driver.findElements(faqAnswer).get(index).getText();
+    }
+
+    public void clickScooterLogo() {
+        driver.findElement(scooterLogo).click();
+    }
+
+    public void clickYandexLogo() {
+        driver.findElement(yandexLogo).click();
+    }
+
+    public void searchOrderStatus(String orderNumber) {
+        driver.findElement(orderNumberField).sendKeys(orderNumber);
+        driver.findElement(goButton).click();
+    }
+
+    public OrderStatusPage checkOrderStatus(String orderNumber) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(orderNumberField)); // Ожидаем появления формы
+
+        driver.findElement(orderNumberField).sendKeys(orderNumber); // Вводим номер заказа
+
+        driver.findElement(goButton).click(); // Нажимаем кнопку Go!
+
+        wait.until(ExpectedConditions.urlContains("/track"));  // Ждем загрузки страницы статуса
+
+        return new OrderStatusPage(driver);
+    }
+
+    public void clickStatusOrderButton() {
+        driver.findElement(statusOrderButton).click();
+    }
+
+    public void setFieldOrderNumber(String orderNumber) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(orderNumberField)).sendKeys(orderNumber);
+    }
+
+    public void clickGoButton() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(goButton)).click();
+    }
 }
--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -1,4 +1,79 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class OrderPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    // Локаторы первой страницы заказа (Для кого самокат)
+    private final By nameInput = By.xpath("//input[@placeholder='* Имя']");
+    private final By surnameInput = By.xpath("//input[@placeholder='* Фамилия']");
+    private final By addressInput = By.xpath("//input[@placeholder='* Адрес: куда привезти заказ']");
+    private final By metroInput = By.xpath("//input[@placeholder='* Станция метро']");
+    private final By phoneInput = By.xpath("//input[@placeholder='* Телефон: на него позвонит курьер']");
+    private final By nextButton = By.xpath("//button[text()='Далее']");
+
+    // Локаторы второй страницы заказа (Про аренду)
+    private final By dateInput = By.xpath("//input[@placeholder='* Когда привезти самокат']");
+    private final By rentalPeriod = By.xpath("//div[text()='* Срок аренды']");
+    private final By colorBlack = By.id("black");
+    private final By colorGrey = By.id("grey");
+    private final By commentInput = By.xpath("//input[@placeholder='Комментарий для курьера']");
+    private final By orderButton = By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM' and text()='Заказать']");
+    private final By confirmButton = By.xpath("//button[text()='Да']");
+    private final By successMessage = By.xpath("//div[text()='Заказ оформлен']");
+
+    public OrderPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    public void fillFirstPage(String name, String surname, String address, String metro, String phone) {
+        wait.until(ExpectedConditions.elementToBeClickable(nameInput)).sendKeys(name);
+        wait.until(ExpectedConditions.elementToBeClickable(surnameInput)).sendKeys(surname);
+        wait.until(ExpectedConditions.elementToBeClickable(addressInput)).sendKeys(address);
+
+        wait.until(ExpectedConditions.elementToBeClickable(metroInput)).click();
+        if (metro != null && !metro.isEmpty()) {
+            By metroOption = By.xpath(String.format("//div[text()='%s']", metro));
+            wait.until(ExpectedConditions.elementToBeClickable(metroOption)).click();
+        }
+
+        wait.until(ExpectedConditions.elementToBeClickable(phoneInput)).sendKeys(phone);
+        wait.until(ExpectedConditions.elementToBeClickable(nextButton)).click();
+    }
+
+    public void fillSecondPage(String date, String period, String color, String comment) {
+        wait.until(ExpectedConditions.elementToBeClickable(dateInput)).sendKeys(date);
+        driver.findElement(By.xpath("//body")).click(); // Клик в любое место, чтобы закрыть календарь
+
+        wait.until(ExpectedConditions.elementToBeClickable(rentalPeriod)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath(String.format("//div[text()='%s']", period)))).click();
+
+        if (color.equals("чёрный жемчуг")) {
+            wait.until(ExpectedConditions.elementToBeClickable(colorBlack)).click();
+        } else {
+            wait.until(ExpectedConditions.elementToBeClickable(colorGrey)).click();
+        }
+
+        wait.until(ExpectedConditions.elementToBeClickable(commentInput)).sendKeys(comment);
+        wait.until(ExpectedConditions.elementToBeClickable(orderButton)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(confirmButton)).click();
+    }
+
+    public boolean isSuccessMessageDisplayed() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successMessage));
+        return driver.findElement(successMessage).isDisplayed();
+    }
+
+    public boolean isNameErrorDisplayed() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[contains(text(), 'Введите корректное имя')]"))).isDisplayed();
+    }
+
 }
